812bcfb56a7e6a492f06bf91cca7f84571f57c24
Sascha Majewsky <sascha.majewsky@pm.me>
2021-07-18 15:56:02 +0200

    Added reusable loading spinner component

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
+import Link from "next/link";
+import Loader from "../components/Loader";
+
 export default function Home() {
   return (
     <div>
+      <Loader show />
       <Link
         prefetch={true}
         href={{
